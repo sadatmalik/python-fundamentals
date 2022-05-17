0eb0fc30b7d7b338b44ccf323dfed828edb9decc
sadat.malik <sadat.malik@me.com>
2022-05-17 17:28:33 +0100

    lab 07_06

--- 07_variables-and-types/07_06_whats_my_type.py
+++ 07_variables-and-types/07_06_whats_my_type.py
@@ -2,6 +2,10 @@
 # What data types does it hold at certain points earlier in the script?
 
 mystery = None
+print(type(mystery)) # NoneType
 mystery = "Sommerfeld"
+print(type(mystery)) # str
 mystery = 137
+print(type(mystery)) # int
 mystery = mystery + 0.0
+print(type(mystery)) # float
