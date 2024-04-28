76fce1d2d9037819a29ab6fdbb7c3567603b0c95
sadatmalik <sadat.malik@me.com>
2024-04-28 17:09:25 +0100

    first lab

--- 04_build-something/guess.py
+++ 04_build-something/guess.py
@@ -8,3 +8,17 @@
 # Type the whole code out instead of copy-pasting.
 # Typing out code, even if you just copy it, trains your coding skills!
 # Write your code below:
+import random
+
+num = random.randint(1, 10)
+guess = None
+
+while guess != num:
+    guess = input("guess a number between 1 and 10: ")
+    guess = int(guess)
+
+    if guess == num:
+        print("congratulations! you got it!")
+        break
+    else:
+        print("nope, try again!")
