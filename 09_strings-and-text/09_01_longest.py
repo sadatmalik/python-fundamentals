f5992be1a1a96cdb4df4b117fe8ef69dde288a76
sadatmalik <sadat.malik@me.com>
2024-07-16 21:41:18 +0100

    longest string

--- 09_strings-and-text/09_01_longest.py
+++ 09_strings-and-text/09_01_longest.py
@@ -5,3 +5,13 @@ longest_german_word = "Donaudampfschifffahrtsgesellschaftskapitänskajütentürs
 longest_hungarian_word = "Megszentségteleníthetetlenségeskedéseitekért"
 longest_finnish_word = "Lentokonesuihkuturbiinimoottoriapumekaanikkoaliupseerioppilas"
 strong_password = "%8Ddb^ca<*'{9pur/Y(8n}^QPm3G?JJY}\(<bCGHv^FfM}.;)khpkSYTfMA@>N"
+
+strings = [longest_finnish_word, longest_german_word, strong_password, longest_hungarian_word]
+
+longest = strings[0]
+
+for string in strings:
+    if len(string) > len(longest):
+        longest = string
+
+print("Longest string is %s", longest)
