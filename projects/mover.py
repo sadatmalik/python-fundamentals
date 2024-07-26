686251cf428cdd5b30667f4a76f7d74362573630
sadatmalik <sadat.malik@me.com>
2024-07-26 23:55:43 +0100

    desktop tidy script

--- projects/mover.py
+++ projects/mover.py
@@ -2,13 +2,23 @@
 # from one folder to another
 
 # Import pathlib
-
-# Find the path to my Desktop
+from pathlib import Path
 
 # Create a new folder
+screenshot_dir = Path.home() / "Desktop" / "Screenshots"
+screenshot_dir.mkdir(exist_ok=True)
 
-# Filter for screenshots only
+# Find the path to my Desktop
+desktop = Path.home() / "Desktop"
+for filepath in desktop.iterdir():
+    # Filter for screenshots only
+    if (filepath.suffix == ".png" and str(filepath.name).startswith("Screenshot")):
+        print(f"Found: {filepath}")
 
-# Create a new path for each file
+        # Create a new path for each file
+        new_path = Path(screenshot_dir / filepath.name)
 
-# Move the screenshot there
+        # Move the screenshot there
+        print(f"Moving to: {new_path}...")
+        filepath.replace(new_path)
+        print(f"Moved.")
